f11bcf37ffb3730cad8d69de4f9ab857a8c635a7
regencode <thomgozalie@gmail.com>
2025-10-02 09:42:47 +0700

    feat: update

--- segmentation_utils.py
+++ segmentation_utils.py
@@ -1,68 +1,77 @@
-from numpy.core.multiarray import ndarray
+import numpy
 import torch, torchvision
 import torch.utils.data
 import typing, torchtyping
 import csv
 import tifffile
-from typing import Any, Callable
+from typing import Any, Callable, List
 import torch.nn.functional as F
 import numpy as np
 import os
 
 
 Callback = Callable[[Any], Any]
+Path = os.PathLike
 
 class SequentialPipeline():
+    '''
+         Apply transforms sequentially to one data,
+         where each transform may result in n data,
+         finally outputting one or more data. (1 to n>=1)
+         Make sure each action can be executed as a function
+         *args will be ignored for the sake of GroupedOperation
+    '''
     def __init__(self,  action_list: tuple[Callback]):
         self.action_list = action_list
 
-    def __call__(self, *data: Any) -> tuple[Any, ...]:
-        # Apply transforms sequentially
-        # Make sure each action can be executed as a function
+    def __call__(self, data: Any, *args) -> tuple[Any, ...]:
+        if not isinstance(data, tuple):
+            data = (data,)  # wrap into tuple
         for action in self.action_list:
             data = action(data)
         return data
 
+class OneToManyOperation():
+    '''
+    Apply a list of actions (n actions) to a single data (1 data),
+    returning multiple data (n data)
+    *args is discarded.
+    '''
+    def __init__(self, each_action):
+        self.each_action = each_action
+
+    def __call__(self, data, *args):
+        new_data = []
+        for action in self.each_action:
+            new_data.append(action(data))
+        return new_data
+
+class GroupedOperation():
+    '''
+    Apply the same action (1 action) to a group of data (n_data),
+    returning mutiple data (n_data)
+    __call__ is variadic.
+    '''
+    def __init__(self, action: Callback):
+        self.action = action
+
+    def __call__(self, *data: Any):
+        data = self.action(data)
+        return data
 
 class InMemoryDataset(torch.utils.data.Dataset):
-    def __init__(self, X: torch.Tensor, y: torch.Tensor,
-                 transforms: SequentialPipeline | None = None, 
-                 X_transforms: SequentialPipeline | None = None, 
-                 y_transforms: SequentialPipeline | None = None, 
-                 pair_transforms: SequentialPipeline | None = None):
-
+    def __init__(self, X: List[Path], y: List[Path]):
         self.X = X
         self.y = y
-        self.transforms = transforms
-        self.X_transforms=  X_transforms
-        self.y_transforms = y_transforms
-        self.pair_transforms = pair_transforms
 
-        if self.X_transforms is None: # default behavior is self.transforms
-            self.X_transforms = transforms
-        if self.y_transforms is None:
-            self.y_transforms = transforms
-
-    def __getitem__(self, i: int) -> tuple[torch.Tensor, torch.Tensor]:
-        x_data = self.X[i]
-        y_data = self.y[i]
-        
-        x_data = self.X_transforms(x_data) if self.X_transforms is not None else x_data
-        y_data = self.y_transforms(y_data) if self.y_transforms is not None else y_data
-        if self.pair_transforms is not None:
-            x_data, y_data = self.pair_transforms(x_data, y_data)
-
-        # Convert numpy arrays to tensors
-        x_data = torch.from_numpy(x_data).float()
-        y_data = torch.from_numpy(y_data).long() # Labels are likely integers
-        return x_data, y_data
+    def __getitem__(self, i: int) -> tuple[Path, Path]:
+        return self.X[i], self.y[i]
 
     def __len__(self) -> int :
         return len(self.X)
 
-
-def load_csv_data(root_path: os.PathLike, 
-                  input_folder_path: os.PathLike[str], 
+def load_csv_data(root_path: os.PathLike,
+                  input_folder_path: os.PathLike[str],
                   label_folder_path: os.PathLike[str]) -> tuple[list[str], list[str]]:
     # Return (X, y) tuple where X and y are list of img and their label in .tiff format
     X, y = [], []
@@ -75,9 +84,14 @@ def load_csv_data(root_path: os.PathLike,
             y.append(label)
     return X, y
 
-def read_from_tiff(tiff_path: str) -> np.ndarray:
-    img = tifffile.imread(tiff_path)
-    return img
+class TiffReader:
+    def __init__(self, path_prefix: str = ""):
+        self.path_prefix = path_prefix
+    def __call__(self, tiff_path: str) -> np.ndarray:
+        if self.path_prefix is not None:
+            tiff_path = os.path.join(self.path_prefix, tiff_path)
+        img = tifffile.imread(tiff_path)
+        return img
 
 def identity(data: Any) -> Any: # for empty pipeline
     return data
